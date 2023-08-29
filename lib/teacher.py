d1f4e6b5f7cf03509f5bbcd33d6f46ec6946eaf1
vivian waithera <vivyannekori@gmail.com>
2023-08-29 12:55:26 +0300

    Completed assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,10 @@ import random
 
 class Teacher(User):
 
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Subject 1", "Subject 2", "Subject 3"]
+
     def teach(self):
-        pass
+        return random.choice(self.knowledge) 
+        
